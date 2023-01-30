f47f850e803e754a4239784b45e8d457a0cd7aff
nauLvero <naulvero@gmail.com>
2023-01-30 12:28:16 +0100

    Added MachineProgram in class  MachineController

--- main.cpp
+++ main.cpp
@@ -9,23 +9,31 @@ private:
 
 public:
     Brush(){};
-    virtual bool elementHandler()
+    virtual bool elementHandler(bool p_handler)
     {
         isBrushOn = !isBrushOn;
         cout << "Brush";
         if (isBrushOn == true)
         {
-            cout << " is on" << endl;
+            cout << " turned on" << endl;
         }
         else
         {
-            cout << " is off" << endl;
+            cout << " turned off" << endl;
         };
         return isBrushOn;
     };
-    void showBrushState()
+    virtual bool showState()
     {
-        cout << "Brush state: " << isBrushOn << endl;
+        if (isBrushOn)
+        {
+            cout << "Brush is on" << endl;
+        }
+        else
+        {
+            cout << "Brush is off" << endl;
+        };
+        return isBrushOn;
     };
 };
 class Roller
@@ -35,9 +43,9 @@ private:
     float rollerVelocity = 0; //[m/min]
 
 public:
-    virtual bool elementHandler()
+    virtual bool elementHandler(bool p_handler)
     {
-        isRollerOn = !isRollerOn;
+        isRollerOn = p_handler;
         cout << "Roller";
         if (isRollerOn == true)
         {
@@ -49,40 +57,84 @@ public:
         };
         return isRollerOn;
     };
+    virtual bool showState()
+    {
+        return isRollerOn;
+    };
 };
 class Sensor
 {
 private:
-    ;
+    bool isSensorOn = true;
 
 public:
-    ;
+    virtual bool elementHandler(bool p_handler)
+    {
+        isSensorOn = p_handler;
+        return isSensorOn;
+    };
+    virtual bool showState()
+    {
+        if (isSensorOn)
+        {
+            cout << "Sensor is on" << endl;
+        }
+        else
+            cout << "Sensor is off" << endl;
+        return isSensorOn;
+    };
 };
-class MashineController
+class MachineController
 {
 private:
     vector<Brush> m_brush;
+    vector<Roller> m_roller;
+    vector<Sensor> m_sensor;
 
 public:
-    MashineController(vector<Brush> p_brush)
+    MachineController(vector<Brush> p_brush, vector<Roller> p_roller, vector<Sensor> p_sensor)
     {
         m_brush = p_brush;
+        m_roller = p_roller;
+        m_sensor = p_sensor;
     };
-    void showBrushState()
+
+    void machineProgram()
     {
-        m_brush[0].showBrushState();
+        vector<Sensor>::iterator it_1 = m_sensor.begin();
+        for (it_1; it_1 != m_sensor.end(); it_1++)
+        {
+            cout << "Wywolano petle" << (*it_1).showState() << endl;
+        };
+
+        if (m_sensor[0].showState())
+        {
+
+            m_brush[0].elementHandler(true);
+            m_roller[0].elementHandler(true);
+        }
+        else if (!m_sensor[0].showState())
+        {
+            m_brush[0].elementHandler(false);
+            m_roller[0].elementHandler(false);
+        }
+        else
+        {
+            cout << "Error - check Sensor 1" << endl;
+        }
     };
 };
 
 int main()
 {
-    Brush b1, b2;
-    Roller r1;
-    vector<Brush> brush = {b1, b2};
-    vector<Roller> v_roller;
-    vector<Sensor> v_Sensor;
-    MashineController m1(brush);
-    m1.showBrushState();
+    Brush b1, b2, b3;
+    Roller r1, r2, r3;
+    Sensor s1, s2, s3;
+    vector<Brush> brush = {b1, b2, b3};
+    vector<Roller> roller = {r1, r2, r3};
+    vector<Sensor> sensor = {s1, s2, s3};
+    MachineController m1(brush, roller, sensor);
+    m1.machineProgram();
     // b1.elementHandler();
     // b2.elementHandler();
     // b2.elementHandler();
